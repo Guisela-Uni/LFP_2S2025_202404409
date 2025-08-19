3923abb79b6f37fb4b448f4585b67764f9500312
Guisela-ai19 <guisellamonroy123@gmail.com>
2025-08-18 18:31:03 -0600

    menu principal

---
+++ main.js
@@ -0,0 +1,13 @@
+//MENU PRINCIPAL
+console.log(" ----------------------- CALL CENTER-----------------------");
+console.log("|          Bienvenido al simulador de Call Center           |");
+console.log("|                                                           |");
+console.log("| 1.Cargar Registros de Llamadas                            |");
+console.log("| 2.Exportar Historial de Llamadas                          |");
+console.log("| 3.Exportar Listado de Operadores                          |");
+console.log("| 4.Exportar Listado de Clietes                             |");
+console.log("| 5.Exportar Rendimiento de Operadores                      |");
+console.log("| 6.Mostrar Porcentaje de Clasificación de Llamadas         |");
+console.log("| 7.Mostrar Cantidad de Llamadas por Calificación           |");
+console.log("| 8.Salir                                                   |.");
+console.log("-----------------------------------------------------------");
